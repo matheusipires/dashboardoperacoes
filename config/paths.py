a990e666fe259ecb80dac3cf9d4e0564622e9d34
Matheus Pires <matheuspires.eng@gmail.com>
2025-05-19 11:36:02 -0300

    Create paths.py

---
+++ config/paths.py
@@ -0,0 +1,7 @@
+# config/paths.py
+
+# Caminho fixo do Excel (não será alterado)
+EXCEL_PATH = r"C:\Users\matheus.pires\OneDrive - ORBIS ENGENHARIA CLINICA\DADOS POWERBI\Painel Operações - Dados.xlsx"
+
+# Nome da aba a ser lida
+SHEET_NAME = "Dados sistemas fechadas"
