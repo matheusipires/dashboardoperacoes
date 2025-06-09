3b40c05903d7fd3203010902e4b4fb2ed2c30588
Matheus Pires <matheuspires.eng@gmail.com>
2025-06-09 11:44:42 -0300

    leitura_dados.py

--- scripts/leitura_dados.py
+++ scripts/leitura_dados.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import gdown
-import os
+import streamlit as st
 
+@st.cache_data
 def carregar_dados():
+    # ID e caminho temporário
     file_id = "1nwiU-O9DNjWGJ2C5PMp65uG2YBVoZnxM"
     output = "/tmp/dados.xlsx"
 
-    # Baixa o arquivo do Google Drive
+    # Download do arquivo
     gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
 
-    # Verifica se o arquivo foi salvo corretamente
-    if not os.path.exists(output):
-        raise FileNotFoundError("Erro ao baixar o arquivo Excel.")
-
-    # Lê a planilha local com engine explícito
+    # Leitura do Excel
     df = pd.read_excel(output, sheet_name="Dados sistemas fechadas", engine="openpyxl")
 
-    # Garante que as colunas de datas estão no formato correto
+    # Conversão de datas
     df['Abertura'] = pd.to_datetime(df['Abertura'], errors='coerce')
     df['Fechamento'] = pd.to_datetime(df['Fechamento'], errors='coerce')
 
